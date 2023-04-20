b1575fb9ef4ad6a58c810bd1cb825cb97bc71f3c
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-04-19 21:49:24 -0500

    Add files via upload

---
+++ src/HornedBeast.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function HornedBeast() {
+  return (
+    <div>
+        
+        <h2>My Header</h2>
+        <img width="100px" src="https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg" alt="Elephant" title="My Elephant is Toejoe"></img>
+    </div>
+  );
+}
+
+export default HornedBeast;
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HornedBeast from './HornedBeast';
+function Main() {
+  return (
+    
+    <div>
+        <HornedBeast/>
+        <h1>My Main title</h1>
+    </div>
+    
+  );
+}
+
+export default Main;
